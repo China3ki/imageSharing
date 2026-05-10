2af31345b1ed28d4e51326fe9a113b75f74f49b8
China3ki <pawelkwasn@gmail.com>
2026-05-10 19:54:53 +0200

    Poprawa header dla wyższej rożdzielczości ekranu + template ekranu głównego

---
+++ imageSharing.Client/Pages/Home.razor.cs
@@ -0,0 +1,23 @@
+﻿using imageSharing.Shared.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace imageSharing.Client.Pages
+{
+    public partial class Home
+    {
+        [Inject]
+        protected ThemeService ThemeService { get; private set; } = default!;
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender) await ThemeService.LoadTheme();
+        }
+        protected override async Task OnInitializedAsync()
+        {
+            ThemeService.OnChange += StateHasChanged;
+        }
+        public void Dispose()
+        {
+            ThemeService.OnChange -= StateHasChanged;
+        }
+    }
+}
