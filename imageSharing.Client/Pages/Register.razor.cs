a51a8f1b48a4ad5fc671db364ffabb75da44af0a
China3ki <pawelkwasn@gmail.com>
2026-05-12 19:46:57 +0200

    Dalsza stylizacja template'u rejestracji

--- imageSharing.Client/Pages/Register.razor.cs
+++ imageSharing.Client/Pages/Register.razor.cs
@@ -1,15 +1,31 @@
 ﻿using imageSharing.Shared.Dto;
+using imageSharing.Shared.Services;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 
 namespace imageSharing.Client.Pages
 {
     public partial class Register
     {
+        [Inject]
+        protected ThemeService ThemeService { get; set; } = default!;
         private RegisterDto _loginDto = new();
-        private EditContext? _loginContext;
+        private EditContext? _loginContext;       
         public Register()
         {
             _loginContext = new(new RegisterDto());
+        }
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender) await ThemeService.LoadTheme();
+        }
+        protected override async Task OnInitializedAsync()
+        {
+            ThemeService.OnChange += StateHasChanged;
+        }
+        public void Dispose()
+        {
+            ThemeService.OnChange -= StateHasChanged;
         }
     }
 }
